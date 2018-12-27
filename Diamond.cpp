d7b98ed217d6cb805a21c08b7f6fee788bc7a8b2
Angphurba Sherpa <angphurba.sherpa@outlook.com>
2018-12-27 10:08:59 -0500

    Add files via upload

---
+++ Diamond.cpp
@@ -0,0 +1,62 @@
+// ConsoleApplication80.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	int size;
+	cout << "Enter size of Diamond: ";
+	cin >> size;
+
+	int z = 1;
+
+	for (int i = 0; i <= size; i++)
+	{
+		for (int j = size; j>i; j--)
+		{
+			cout << " "; // printing space here
+		}
+
+		cout << "*"; // printing asterisk here
+
+		if (i>0)
+		{
+			for (int k = 1; k <= z; k++)
+			{
+				cout << " ";
+			}
+			z += 2;
+			cout << "*";
+		}
+		cout << endl; // end line similar to n
+	}
+
+	z -= 4;
+
+	for (int i = 0; i <= size - 1; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << " ";
+		}
+
+		cout << "*";
+
+		for (int k = 1; k <= z; k++)
+		{
+			cout << " ";
+		}
+		z -= 2;
+
+		if (i != size - 1)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	system("pause");
+	return 0;
+}
+
